3cc7da221bdf9324e89876d5fb1bbe026c88512e
Maria Letícia do Nascimento Gaspar <mleticiagaspar17@gmail.com>
2026-09-20 18:14:26 +0200

    feat(auth): add user entity

---
+++ backend/src/main/java/com/QuadGPT/backend/auth/entity/User.java
@@ -0,0 +1,16 @@
+package com.QuadGPT.backend.auth.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+}
